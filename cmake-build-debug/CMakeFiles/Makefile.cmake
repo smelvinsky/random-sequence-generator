3c914d248586a16ed29d9191e0408d977b9f529b
smelvinsky <damian.a.smela@gmail.com>
2017-10-22 15:42:29 +0200

    Change the project name
    RandomSequence instead of RandomNumber will be more accurate.

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -44,5 +44,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/RandomNumberGenerator.dir/DependInfo.cmake"
+  "CMakeFiles/RandomSequenceGenerator.dir/DependInfo.cmake"
   )
---
+++ cmake-build-debug/CMakeFiles/RandomSequenceGenerator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RandomSequenceGenerator.dir/src/main.c.o"
+  "RandomSequenceGenerator.pdb"
+  "RandomSequenceGenerator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/RandomSequenceGenerator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
